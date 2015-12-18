8b34eee50d54f378e852980f6f8a40a27d637397
Jason Liu <jaliu@sierrawireless.com>
2015-12-18 10:21:26 +0800

    DEV86018 - Add CRC check and calculation for SMEM messages and usb description areas
    Issue Description:
    There is no CRC for SMEM(shared memory) messages(BS_SMEM_REGION_MSG) and usb description(BS_SMEM_REGION_USBD) areas. This ticket is to add the CRC check and calculation for these two areas.
    Note: There is also no CRC for error area(BS_SMEM_REGION_ERR). The error information is just for debug. No big deal even though bit flip happened. Restore to default value will remove all useful information for debug if CRC used and data change happened.
    
    Root cause:
    New feature.
    
    Solution:
    1) Porting the CRC check and calculation for SMEM messages from [DEV82549] [TRUNK] add CRC check for SMEM messages.
    2) Add CRC check and calculation for SMEM usb description area.
    
    Change-Id: Idd635a6b6b0e2921a894a25af0d97fa1337255c2
    Signed-off-by: Jason Liu <jaliu@sierrawireless.com>

---
+++ app/aboot/sierra_bludefs.h
@@ -0,0 +1,20 @@
+/************
+ *
+ * Filename:  bludefs.h
+ *
+ * Purpose:   external definitions for BL package
+ *
+ * NOTES:
+ *
+ * Copyright: (C) 2015 Sierra Wireless, Inc.
+ *            All rights reserved
+ *
+ ************/
+
+#ifndef bludefs_h
+#define bludefs_h
+
+extern int sierra_smem_boothold_mode_set();
+extern bool sierra_is_fastboot_disabled(void);
+
+#endif /* bludefs_h */
