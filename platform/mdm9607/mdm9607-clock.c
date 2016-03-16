a81b5a2530d1aa35924dc5a1ca8df031b755ba92
Xin Wang <xwang@sierrawireless.com>
2016-03-16 14:22:45 +0800

    DEV87968 Enable UART2 as debug port/console port
    Issue Description:
    Enable UART2 as debug port/console port
    
    Root cause:
    N/A
    
    Solution:
    Enable UART2 as debug port/console port:
    - Print SBL log
    - Print LK log
    - Print Kernel log
    - Kernel shell over uart.
    
    Change-Id: If528b7e35ba9dcfee82b0a855853764ac7c03d97
    Signed-off-by: Xin Wang <xwang@sierrawireless.com>
    (cherry picked from commit 29d7ce6e1fcb6ca95532effac1f8558fdade02b2)
    
    Conflicts:
    
    	makefile
    
    Signed-off-by: Ryan Kirk <rkirk@sierrawireless.com>

--- platform/mdm9607/mdm9607-clock.c
+++ platform/mdm9607/mdm9607-clock.c
@@ -111,6 +111,8 @@ static struct pll_vote_clk gpll0_clk_src =
 	},
 };
 
+/* SWISTART */
+#ifndef SIERRA
 /* UART Clocks */
 static struct clk_freq_tbl ftbl_gcc_blsp1_2_uart5_apps_clk[] =
 {
@@ -160,6 +162,58 @@ static struct branch_clk gcc_blsp1_uart5_apps_clk =
 		.ops      = &clk_ops_branch,
 	},
 };
+#else /* SIERRA */
+/* UART Clocks */
+static struct clk_freq_tbl ftbl_gcc_blsp1_2_uart1_apps_clk[] =
+{
+       F( 3686400,  gpll0,    1,  72,  15625),
+       F( 7372800,  gpll0,    1, 144,  15625),
+       F(14745600,  gpll0,    1, 288,  15625),
+       F(16000000,  gpll0,   10,   1,      5),
+       F(19200000,    cxo,    1,   0,      0),
+       F(24000000,  gpll0,    1,   3,    100),
+       F(25000000,  gpll0,   16,   1,      2),
+       F(32000000,  gpll0,    1,   1,     25),
+       F(40000000,  gpll0,    1,   1,     20),
+       F(46400000,  gpll0,    1,  29,    500),
+       F(48000000,  gpll0,    1,   3,     50),
+       F(51200000,  gpll0,    1,   8,    125),
+       F(56000000,  gpll0,    1,   7,    100),
+       F(58982400,  gpll0,    1,1152,  15625),
+       F(60000000,  gpll0,    1,   3,     40),
+       F_END
+};
+
+static struct rcg_clk blsp1_uart1_apps_clk_src =
+{
+       .cmd_reg      = (uint32_t *) BLSP1_UART1_APPS_CMD_RCGR,
+       .cfg_reg      = (uint32_t *) BLSP1_UART1_APPS_CFG_RCGR,
+       .m_reg        = (uint32_t *) BLSP1_UART1_APPS_M,
+       .n_reg        = (uint32_t *) BLSP1_UART1_APPS_N,
+       .d_reg        = (uint32_t *) BLSP1_UART1_APPS_D,
+
+       .set_rate     = clock_lib2_rcg_set_rate_mnd,
+       .freq_tbl     = ftbl_gcc_blsp1_2_uart1_apps_clk,
+       .current_freq = &rcg_dummy_freq,
+
+       .c = {
+               .dbg_name = "blsp1_uart1_apps_clk",
+               .ops      = &clk_ops_rcg_mnd,
+       },
+};
+
+static struct branch_clk gcc_blsp1_uart1_apps_clk =
+{
+       .cbcr_reg     = (uint32_t *) BLSP1_UART1_APPS_CBCR,
+       .parent       = &blsp1_uart1_apps_clk_src.c,
+
+       .c = {
+               .dbg_name = "gcc_blsp1_uart1_apps_clk",
+               .ops      = &clk_ops_branch,
+       },
+};
+#endif /* SIERRA */
+/* SWISTOP */
 
 static struct vote_clk gcc_blsp1_ahb_clk = {
 	.cbcr_reg     = (uint32_t *) BLSP1_AHB_CBCR,
@@ -269,8 +323,15 @@ static struct vote_clk gcc_ce1_axi_clk = {
 /* Clock lookup table */
 static struct clk_lookup mdm_clocks_9607[] =
 {
+/* SWISTART */
+#ifndef SIERRA
 	CLK_LOOKUP("uart5_iface_clk", gcc_blsp1_ahb_clk.c),
 	CLK_LOOKUP("uart5_core_clk",  gcc_blsp1_uart5_apps_clk.c),
+#else /* SIERRA */
+	CLK_LOOKUP("uart1_iface_clk", gcc_blsp1_ahb_clk.c),
+	CLK_LOOKUP("uart1_core_clk",  gcc_blsp1_uart1_apps_clk.c),
+#endif /* SIERRA */
+/* SWISTOP */ 
 
 	CLK_LOOKUP("usb_iface_clk",  gcc_usb_hs_ahb_clk.c),
 	CLK_LOOKUP("usb_core_clk",   gcc_usb_hs_system_clk.c),
