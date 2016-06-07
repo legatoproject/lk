776a5251445537f20b5f5fc08acaca4ef6627ec1
Kinbo Huang <KHuang@sierrawireless.com>
2016-06-07 17:14:55 +0800

    [9x28] Reset management for 9x28
    Issue Description:
    This check in is to fix the issue QTI9X28-209.
    To avoid modem continuous abnormal reset,
    need reset management mechanism as legency platforms to manage the reset counter,
    and record different reset types;
    
    Root cause:
    New request.
    
    Solution:
    Implement reset management to record reset count/different reset types, and take according actions.
    For example, modem enter fastboot mode in LK if fatal error counter >= 6.
    Port from 9x40.
    
    Change-Id: Ib50ec42fb0c0d06252881e602b6d94b3fd196ae6
    Signed-off-by: Kinbo Huang <KHuang@sierrawireless.com>
    (cherry picked from commit 8417dac98f8bccf4393c1da756b82e778b5ace97)
    Signed-off-by: Ryan Kirk <rkirk@sierrawireless.com>

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -94,7 +94,10 @@
 #include <display_menu.h>
 #include "fastboot_test.h"
 /* SWISTART */
+#ifdef SIERRA
+#include "mach/sierra_smem.h"
 #include "sierra_bludefs.h"
+#endif
 /* SWISTOP */
 
 extern  bool target_use_signed_kernel(void);
@@ -2687,6 +2690,15 @@ void cmd_erase_mmc(const char *arg, void *data, unsigned sz)
 
 void cmd_erase(const char *arg, void *data, unsigned sz)
 {
+/* SWISTART */
+#ifdef SIERRA
+	/* clear error reset count */
+	sierra_smem_err_count_set(0);
+	/* set reset type to BS_BCMSG_RTYPE_SW_UPDATE_IN_LK */
+	sierra_smem_reset_type_set(BS_BCMSG_RTYPE_SW_UPDATE_IN_LK);
+#endif
+/* SWISTOP */
+
 #if VERIFIED_BOOT
 	if (target_build_variant_user())
 	{
@@ -3299,6 +3311,7 @@ void cmd_flash_nand(const char *arg, void *data, unsigned sz)
 	unsigned extra = 0;
 	uint64_t partition_size = 0;
 /* SWISTART */
+#ifdef SIERRA
 	enum blresultcode ret = BLRESULT_OK;
 
 	if (!strcmp(arg, "sierra"))
@@ -3355,6 +3368,7 @@ void cmd_flash_nand(const char *arg, void *data, unsigned sz)
 	}
 	else
 	{
+#endif
 /* SWISTOP */
 	ptable = flash_get_ptable();
 	if (ptable == NULL) {
@@ -3411,13 +3425,24 @@ void cmd_flash_nand(const char *arg, void *data, unsigned sz)
 	}
 	dprintf(INFO, "partition '%s' updated\n", ptn->name);
 /* SWISTART */
+#ifdef SIERRA
 	}
+#endif
 /* SWISTOP */
 	fastboot_okay("");
 }
 
 void cmd_flash(const char *arg, void *data, unsigned sz)
 {
+/* SWISTART */
+#ifdef SIERRA
+	/* clear error reset count */
+	sierra_smem_err_count_set(0);
+	/* set reset type to BS_BCMSG_RTYPE_SW_UPDATE_IN_LK */
+	sierra_smem_reset_type_set(BS_BCMSG_RTYPE_SW_UPDATE_IN_LK);
+#endif
+/* SWISTOP */
+
 	if(target_is_emmc_boot())
 		cmd_flash_mmc(arg, data, sz);
 	else
@@ -4083,6 +4108,15 @@ void aboot_init(const struct app_descriptor *app)
 	}
 #endif
 #endif
+
+/* SWISTART */
+#ifdef SIERRA
+	if (sierra_if_enter_fastboot())
+	{
+		boot_into_fastboot = true;
+	}
+#endif
+/* SWISTOP */
 
 normal_boot:
 	if (!boot_into_fastboot)
