c329e8177623de1f7cef884ba43f79a7754e2b9d
ceasar guo <cguo@sierrawireless.com>
2016-11-09 10:48:52 +0800

    Service pin to enter fastboot
    The default function of service pin is defined to decide whether enter emergency download mode.
    now 9x28 plans to use this to enter fastboot mode instead of emergency download mode.
    
    In fuse GPIO_37:Force USB Boot is enabled by default, it must be disabled in the first place.
    Use AT command : AT!BSFUSETEST=000A0150,08
    
    Resolves:QTI9X28-599
    Change-Id: Iec3fc4f5be1e002a23b51eec1b4fa55802a000fc
    Signed-off-by: ceasar guo <cguo@sierrawireless.com>

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -95,6 +95,9 @@
 #include "sierra_bludefs.h"
 #include "sierra_dsudefs.h"
 #include "sierra_secudefs.h"
+
+/* Sense service pin to decide whether enter fastboot mode */
+#define NO_KEYPAD_DRIVER 1
 #endif
 /* SWISTOP */
 
@@ -3533,8 +3536,11 @@ void aboot_init(const struct app_descriptor *app)
 			boot_into_fastboot = true;
 	}
 	#if NO_KEYPAD_DRIVER
-	if (fastboot_trigger())
+    if (board_hardware_subtype() == SWI_AR_BOARD)
+    {
+	  if (fastboot_trigger())
 		boot_into_fastboot = true;
+    }
 	#endif
 
 #if USE_PON_REBOOT_REG
