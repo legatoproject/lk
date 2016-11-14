5de92db1e69d6558d3fbe92bb0a70373960950f8
Lucy He <LHe@sierrawireless.com>
2016-11-14 21:06:05 +0800

    Add the "BOOTQUIETDISABLE" NV and AT command
    Issue Description:
    Add a NV and AT command to control whether show the
    kernel boot message.
    
    Root Cause:
    N/A
    
    Solution:
    Add a bcbootquiet byte to bccoworkmsg struct for
    "BOOTQUIETDISABLE", and Read it to update cmdline.
    When the "BOOTQUIETDISABLE" was set 0, the commandline
    will have a "quiet" paramter, the kernel bootup message
    will be hide. When the "BOOTQUIETDISABLE" was set 1, the
    kernel bootup message will be show.
    
    Resolves: QTI9X40-1090
    Change-Id: I71c1f50982a55daccfe00edb125d4083e1527c97
    Signed-off-by: Lucy He <LHe@sierrawireless.com>

--- app/aboot/sierra_bludefs.h
+++ app/aboot/sierra_bludefs.h
@@ -324,7 +324,7 @@ extern boolean swipart_get_logical_partition_from_backup(
   backup_logical_partition_type logical_partition,
   uint32 *start_block,
   uint32 *end_block);
-
+extern bool sierra_is_bootquiet_disabled(void);
 enum blresultcode blprocessdldcontinue(uint8 *payloadp, uint32 tlen, uint32 *bytesleftp);
 enum blresultcode blprocessdldend(void);
 enum blresultcode blprocessdldstart(uint8 *cwehdrp, uint32 tlen);
